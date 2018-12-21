4e4116ae2456228f69e0ceeba148dfa929563100
Greg Morgan <gregm9999@gmail.com>
2018-12-21 23:06:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,7 @@ class StudentsController < ApplicationController
     @student=set_student
     @student.active=!@student.active
     @student.save
+    binding.pry
     redirect_to student_path(@student)
   end
 
